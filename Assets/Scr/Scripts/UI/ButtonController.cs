048764d665bde28995234b621c596d7cfe04f1a0
Ricardo Orozco <ricardo.orozcoext@holcim.com>
2024-04-19 14:48:16 -0500

    add fx sound to buttons

--- Assets/Scr/Scripts/UI/ButtonController.cs
+++ Assets/Scr/Scripts/UI/ButtonController.cs
@@ -1,14 +1,29 @@
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class ButtonController : MonoBehaviour
 {
+    [SerializeField]
+    private AudioSource buttonFxSounds;
+
+    [SerializeField]
+    private AudioClip hoverSound;
+
+    private void Awake()
+    {
+        buttonFxSounds = GetComponent<AudioSource>();
+        buttonFxSounds.volume = 0.2f;
+    }
+
     public void mouseOverFx()
     {
         LeanTween.scale(this.gameObject, new Vector3(1.2f, 1.2f, 1.2f), 0.1f);
+        buttonFxSounds.PlayOneShot(hoverSound);
     }
     public void mouseOverFx2(float interval)
     {
         LeanTween.scale(this.gameObject, new Vector3(1.2f, 1.2f, 1.2f), interval).setLoopPingPong();
+        buttonFxSounds.PlayOneShot(hoverSound);
     }
 
     public void mouseExitFx()
